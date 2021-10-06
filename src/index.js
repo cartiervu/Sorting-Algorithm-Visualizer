ecb2134351480421ffa7539108d6bd4ecae4754f
Cartier Vu <vu.cartier@gmail.com>
2021-10-05 22:54:41 -0700

    Implemented Visual Bubble Sort Algorithm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,68 @@
 import _, { sortBy } from 'lodash';
 import './style.css'
 
+const SLEEP_TIME_MS = 250;
+const NUMBER_OF_ELEMENTS = 30;
+
 let array = [];
+
+const visualizeSort = () => {
+    bubbleSort();
+}
+
+const visualizeSwap = (indexOne, indexTwo) => {
+    const blockOne = document.querySelector(`[data-index="${indexOne}"]`);
+    const blockTwo = document.querySelector(`[data-index="${indexTwo}"]`);
+    blockOne.setAttribute('data-index', indexTwo);
+    blockTwo.setAttribute('data-index', indexOne);
+
+    swap(indexOne, indexTwo);
+
+    blockTwo.parentNode.insertBefore(blockTwo, blockOne);
+}
+
+async function bubbleSort() {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - i - 1; j++) {
+            highlightBlocks(j, j+1);
+            await sleep(SLEEP_TIME_MS);
+            if (array[j] > array[j+1]) {
+                visualizeSwap(j, j+1);
+                await sleep(SLEEP_TIME_MS);
+            }
+            unhighlightBlock(j);
+            await sleep(SLEEP_TIME_MS);
+            if (j == array.length - i - 2) {
+                highlightCompletedBlock(array.length - i - 1);
+            }
+        }
+    }
+}
+
+const swap = (i, j) => {
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
+
+const highlightBlocks = (indexOne, indexTwo) => {
+    const blockOne = document.querySelector(`[data-index="${indexOne}"]`);
+    const blockTwo = document.querySelector(`[data-index="${indexTwo}"]`);
+
+    blockOne.firstChild.style.backgroundColor = 'tomato';
+    blockTwo.firstChild.style.backgroundColor = 'tomato';
+}
+
+const unhighlightBlock = (index) => {
+    const block = document.querySelector(`[data-index="${index}"]`);
+
+    block.firstChild.style.backgroundColor = 'lightskyblue';
+}
+
+const highlightCompletedBlock = (index) => {
+    const block = document.querySelector(`[data-index="${index}"]`);
+    block.firstChild.style.backgroundColor = 'gold';
+}
 
 const randomizeArray = (numberOfElements) => {
     for (let i = 0; i < numberOfElements; i++) {
@@ -28,23 +89,24 @@ const renderArray = () => {
     const arrayContainer = document.getElementById('array-container');
     removeAllChildNodes(arrayContainer);
 
-    array.forEach((num) => {
+    for (let i = 0; i < array.length; i++) {
         const arrayBlockContainer = document.createElement('div');
         arrayBlockContainer.classList = 'array-block-container';
+        arrayBlockContainer.setAttribute('data-index', i);
         
         const arrayBlock = document.createElement('div');
         arrayBlock.classList = 'array-block';
         arrayBlock.style.width = arrayContainer.offsetWidth / array.length + 'px';
-        arrayBlock.style.height = (num * 10) + 'px';
+        arrayBlock.style.height = (array[i] * 10) + 'px';
 
         const arrayBlockText = document.createElement('p');
         arrayBlockText.classList = 'array-block-text';
-        arrayBlockText.textContent = num;
+        arrayBlockText.textContent = array[i];
         
         arrayBlockContainer.appendChild(arrayBlock);
         arrayBlockContainer.appendChild(arrayBlockText);
         arrayContainer.appendChild(arrayBlockContainer);
-    });
+    }
 }
 
 const removeAllChildNodes = (parent) => {
@@ -53,5 +115,9 @@ const removeAllChildNodes = (parent) => {
     }
 }
 
-randomizeArray(10);
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+randomizeArray(NUMBER_OF_ELEMENTS);
 setup();
